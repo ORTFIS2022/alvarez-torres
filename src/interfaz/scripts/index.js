5850a77f9b53644858fb2eb049d9828790df7ffa
Pablo <pablotolez@hotmail.com>
2022-06-22 19:47:36 -0300

    Revert "arreglando release"
    This reverts commit 945837cf7a1b0947e2a4a3213d55645735d2eaa1.

---
+++ src/interfaz/scripts/index.js
@@ -0,0 +1,70 @@
+import { MDCRipple } from '@material/ripple';
+import { MDCTopAppBar } from '@material/top-app-bar';
+import { MDCTabBar } from '@material/tab-bar';
+import { MDCTextField } from '@material/textfield';
+import { MDCSelect } from '@material/select';
+import {MDCSnackbar} from '@material/snackbar';
+import ListaPeliculas from '../../dominio/lista-peliculas.mjs';
+import Pelicula from '../../dominio/pelicula.mjs';
+
+const listaPeliculas = new ListaPeliculas();
+
+const textFieldTitle = new MDCTextField(document.getElementById('title'));
+const textFieldYear = new MDCTextField(document.getElementById('year'));
+const selectGenre = new MDCSelect(document.querySelector('.mdc-select'));
+
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const botonInicio= new MDCRipple(document.getElementById('home'));
+botonInicio.listen('click', () => {
+  document.body.style.backgroundImage = 'url("../styles/background1.png")';
+  document.querySelectorAll(".content").forEach(element => {
+    element.classList.add("sample-content--hidden");
+  }
+  );
+});
+
+
+const botonMejora= new MDCRipple(document.getElementById('upgradeButton'));
+botonMejora.listen('click', () => {
+  document.body.style.backgroundImage = 'url("../styles/background2.png")';
+  document.querySelectorAll(".content").forEach((element, index) => {
+    if (index === 1) {
+      element.classList.remove("sample-content--hidden");
+    } else {
+      element.classList.add("sample-content--hidden");
+    }
+  });
+});
+
+const botonCuenta = new MDCRipple(document.getElementById('accountButton'));
+botonCuenta.listen('click', () => {
+  document.body.style.backgroundImage = 'url("../styles/background2.png")';
+  document.querySelectorAll(".content").forEach((element, index) => {
+    if (index === 0) {
+      element.classList.remove("sample-content--hidden");
+    } else {
+      element.classList.add("sample-content--hidden");
+    }
+  });
+}
+);
+
+const addButton = new MDCRipple(document.getElementById('addButton'));
+addButton.listen('click', () => {
+  let title = textFieldTitle.value;
+  let year = textFieldYear.value;
+  let genre = selectGenre.value;
+  try {
+    let newPelicula = new Pelicula(title, genre, year);
+    listaPeliculas.agregar(newPelicula);
+  } catch (error) {
+    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
+    snackbar.labelText = error.message;
+    snackbar.open();
+  } finally {
+    let peliculas = listaPeliculas.getPeliculas();
+    console.log(peliculas);
+  }
+})
